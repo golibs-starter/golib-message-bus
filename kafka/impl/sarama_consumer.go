c9db84d57ee38e0471113df81cb36ec1d011e195
Thang Bui <zenthangplus@gmail.com>
2022-09-18 11:06:16 +0700

    Put consume method inside infinity loop

--- kafka/impl/sarama_consumer.go
+++ kafka/impl/sarama_consumer.go
@@ -11,12 +11,13 @@ import (
 )
 
 type SaramaConsumer struct {
-	client        sarama.Client
-	consumerGroup sarama.ConsumerGroup
-	mapper        *SaramaMapper
-	topic         string
-	name          string
-	handler       core.ConsumerHandler
+	client               sarama.Client
+	consumerGroup        sarama.ConsumerGroup
+	consumerHandler      core.ConsumerHandler
+	consumerGroupHandler sarama.ConsumerGroupHandler
+	name                 string
+	topic                string
+	running              bool
 }
 
 func NewSaramaConsumer(
@@ -29,14 +30,13 @@ func NewSaramaConsumer(
 	if err != nil {
 		return nil, errors.WithMessage(err, "Error when create sarama consumer group")
 	}
-
 	return &SaramaConsumer{
-		client:        client,
-		mapper:        mapper,
-		name:          coreUtils.GetStructShortName(handler),
-		handler:       handler,
-		topic:         topicConsumer.Topic,
-		consumerGroup: consumerGroup,
+		client:               client,
+		consumerGroup:        consumerGroup,
+		consumerHandler:      handler,
+		consumerGroupHandler: NewConsumerGroupHandler(handler.HandlerFunc, mapper),
+		name:                 coreUtils.GetStructShortName(handler),
+		topic:                topicConsumer.Topic,
 	}, nil
 }
 
@@ -47,30 +47,36 @@ func (c *SaramaConsumer) Start(ctx context.Context) {
 	// Track errors
 	go func() {
 		for err := range c.consumerGroup.Errors() {
-			log.Errorf("ConsumerGroup error for consumer [%s], detail: [%v]", c.name, err)
+			log.Errorf("Consumer group error for consumer [%s], detail: [%v]", c.name, err)
 		}
 	}()
 
 	// Iterate over consumers sessions.
-	log.Infof("Consumer [%s] with topic [%v] is running", c.name, topics)
-	handler := NewConsumerGroupHandler(c.handler.HandlerFunc, c.mapper)
-	if err := c.consumerGroup.Consume(ctx, topics, handler); err != nil {
-		if err == sarama.ErrClosedConsumerGroup {
-			log.Infof("Consume group [%s] is closed when consume topics [%v], detail [%s]",
-				c.name, topics, err.Error())
-		} else {
-			log.Errorf("Error when consume message in topics [%v] for consumer [%s], detail [%v]",
-				topics, c.name, err)
+	c.running = true
+	for c.running {
+		log.Infof("Consumer [%s] with topic [%v] is running", c.name, topics)
+		if err := c.consumerGroup.Consume(ctx, topics, c.consumerGroupHandler); err != nil {
+			if err == sarama.ErrClosedConsumerGroup {
+				log.Infof("Consume group [%s] is closed when consume topics [%v], detail [%s]",
+					c.name, topics, err.Error())
+			} else if !c.running {
+				log.Infof("Consumer group [%s] is closed when consume topics [%v]",
+					c.name, topics)
+			} else {
+				log.Errorf("Consume group [%s] error when consume topics [%v], error [%v]",
+					c.name, topics, err)
+			}
 		}
 	}
-	log.Infof("Consumer [%s] with topic [%v] is closed", c.name, topics)
+	log.Infof("Consumer group [%s] with topic [%v] is closed", c.name, topics)
 }
 
 func (c *SaramaConsumer) Close() {
 	log.Infof("Consumer [%s] is stopping", c.name)
 	defer log.Infof("Consumer [%s] stopped", c.name)
+	c.running = false
 	if err := c.consumerGroup.Close(); err != nil {
 		log.Errorf("Consumer [%s] could not stop. Error [%v]", c.name, err)
 	}
-	c.handler.Close()
+	c.consumerHandler.Close()
 }
